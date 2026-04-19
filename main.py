9bfc8a1bc98d0c1e994c2249bdff06a572006d97
IFTICENE <chermatti.31@gmail.com>
2026-04-19 07:51:16 -0400

    Test de recherche d'une salle par code via ServiceSalle

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 #etape6 Implémentation de la couche Service (Logique métier)
 
 
-
 from services.service_salle import ServiceSalle
 
 service = ServiceSalle()
 
 try:
-    code = "S21"
+    code = "S2"
 
-    result = service.supprimer_salle(code)
+    salle = service.rechercher_salle(code)
 
-    print(result)
+    if salle:
+        print("Salle trouvée ✔")
+        salle.afficher_infos()
+    else:
+        print("Salle introuvable")
 
 except Exception as e:
     print("Erreur :", e)
