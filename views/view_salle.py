a5896215071546730f6c2ac6fc7da9f8b10aed78
IFTICENE <chermatti.31@gmail.com>
2026-04-19 14:30:29 -0400

    Étape 8.2 Méthode de remplissage de la liste

--- views/view_salle.py
+++ views/view_salle.py
@@ -120,3 +120,12 @@ class ViewSalle(ctk.CTk):
 
             self.capacite_entry.delete(0, 'end')
             self.capacite_entry.insert(0, salle.capacite)
+
+    def lister_salles(self):
+
+        self.treeList.delete(*self.treeList.get_children())
+
+        liste = self.service_salle.recuperer_salles()
+
+        for s in liste:
+            self.treeList.insert("","end",values=(s.code, s.libelle, s.type, s.capacite))
