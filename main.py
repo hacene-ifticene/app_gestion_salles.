889f3d97004bad266b22727d29ac87a1c7b0f611
IFTICENE <chermatti.31@gmail.com>
2026-04-19 07:37:03 -0400

    Test de la méthode modifier_salle dans ServiceSalle

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 #etape6 Implémentation de la couche Service (Logique métier)
 
 
-
 from services.service_salle import ServiceSalle
 from models.salle import Salle
 
 service = ServiceSalle()
 
 try:
-    salle = Salle("S21", "Salle Service", "Laboratoire", 30)
+    salle = Salle("S21", "Salle Modifiée", "TP", 45)
 
-    result = service.ajouter_salle(salle)
+    result = service.modifier_salle(salle)
 
     print(result)
 
 except Exception as e:
-    print("❌ Erreur :", e)
+    print("Erreur :", e)
